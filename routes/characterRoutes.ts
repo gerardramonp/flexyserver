d1f8d5cfc6e3acb293e48cd02d718e5d7dd23981
Gerard Ramon Monte <g.ramon@glofox.com>
2025-02-13 11:48:14 +0100

    Basic authenthication and other fixes

--- routes/characterRoutes.ts
+++ routes/characterRoutes.ts
@@ -2,15 +2,15 @@ import express from "express";
 import Character from "../models/character";
 const router = express.Router();
 
-router.post("/", async (req, res) => {
-  try {
-    const character = new Character(req.body);
-    await character.save();
-    res.status(201).send(character);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
+// router.post("/", async (req, res) => {
+//   try {
+//     const character = new Character(req.body);
+//     await character.save();
+//     res.status(201).send(character);
+//   } catch (error) {
+//     res.status(400).send(error);
+//   }
+// });
 
 router.get("/", async (req, res) => {
   try {
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import mongoose from "mongoose";
 import characterRouter from "./routes/character";
 import dotenv from "dotenv";
@@ -9,25 +9,39 @@ const app = express();
 const port = process.env.PORT || 3000;
 const mongoUri =
   process.env.MONGODB_URI || "mongodb://localhost:27017/flexyserver";
+const apiKey = process.env.API_PRIVATE_KEY;
 
 // Middleware
 app.use(express.json());
 
+const verifyApiKey = (req: Request, res: Response, next: NextFunction) => {
+  const key = req.headers["x-api-key"];
+  if (key && key === apiKey) {
+    return next();
+  } else {
+    return res.status(403).send("Forbidden: Invalid API key");
+  }
+};
+
 // MongoDB connection
-mongoose
-  .connect(mongoUri)
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.error("Failed to connect to MongoDB", err);
-  });
+// mongoose
+//   .connect(mongoUri)
+//   .then(() => {
+//     console.log("Connected to MongoDB");
+//   })
+//   .catch((err) => {
+//     console.error("Failed to connect to MongoDB", err);
+//   });
 
 // Routes
 app.use("/characters", characterRouter);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Running...");
+});
+
+app.get("/private", verifyApiKey, (req: Request, res: Response) => {
+  res.send("valid key...");
 });
 
 // Start server
