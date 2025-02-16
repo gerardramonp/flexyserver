dd5ca71d770563703a3c87f8380d2593ea2ae443
Gerard Ramon Monte <g.ramon@glofox.com>
2025-02-16 21:13:27 +0100

    Implement get all characters

---
+++ src/controllers/characters/getAllCharactersController.ts
@@ -0,0 +1,9 @@
+import { CharacterRepo } from "../../repositories/characters/characterRepo";
+
+export const getAllCharactersController = async () => {
+  const characters = await CharacterRepo.getAll();
+
+  const filtered = characters.filter((char) => char.name === "Dejairzin");
+
+  return filtered;
+};
--- src/routes/characters/public/pubCharacterRoutes.ts
+++ src/routes/characters/public/pubCharacterRoutes.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response } from "express";
+import { getAllCharactersController } from "../../../controllers/characters/getAllCharactersController";
 
 const router = express.Router();
 
 // Get all characters
 router.get("/", async (req: Request, res: Response) => {
   try {
-    res.status(200).send("not implemented");
+    const characters = await getAllCharactersController();
+    res.status(200).json(characters);
   } catch (error) {
     res.status(500).send(error);
   }
