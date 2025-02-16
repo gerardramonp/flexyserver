056cc5c1b2c7d01457f063523b3177eeb8e83869
Gerard Ramon Monte <g.ramon@glofox.com>
2025-02-16 21:17:55 +0100

    Invert filter

--- src/controllers/characters/getAllCharactersController.ts
+++ src/controllers/characters/getAllCharactersController.ts
@@ -3,7 +3,7 @@ import { CharacterRepo } from "../../repositories/characters/characterRepo";
 export const getAllCharactersController = async () => {
   const characters = await CharacterRepo.getAll();
 
-  const filtered = characters.filter((char) => char.name === "Dejairzin");
+  const filtered = characters.filter((char) => char.name !== "Dejairzin");
 
   return filtered;
 };
