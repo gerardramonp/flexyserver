40cbb96daa7cf50bd8fff9a21b7d8772262a04e9
Gerard Ramon Monte <g.ramon@glofox.com>
2025-03-12 16:03:37 +0100

    Remove create user route

--- src/routes/users/userRoutes.ts
+++ src/routes/users/userRoutes.ts
@@ -4,20 +4,20 @@ import loginController from "../../controllers/users/loginController";
 
 const router = express.Router();
 
-router.post("/", async (req: Request, res: Response) => {
-  try {
-    const user = req.body;
+// router.post("/", async (req: Request, res: Response) => {
+//   try {
+//     const user = req.body;
 
-    if (!user.email || !user.username || !user.password) {
-      return res.status(400).send("Missing required fields");
-    }
+//     if (!user.email || !user.username || !user.password) {
+//       return res.status(400).send("Missing required fields");
+//     }
 
-    const newUser = await createUserController(user);
-    res.status(201).json(newUser);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
+//     const newUser = await createUserController(user);
+//     res.status(201).json(newUser);
+//   } catch (error) {
+//     res.status(500).send(error);
+//   }
+// });
 
 router.post("/login", async (req: Request, res: Response) => {
   try {
