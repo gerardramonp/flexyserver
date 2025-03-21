8400bfe002a0ab37324a46d22ea4dd5e89d4c13f
Gerard Ramon Monte <g.ramon@glofox.com>
2025-03-21 10:42:49 +0100

    modify get chars error to json

--- src/routes/characters/public/pubCharacterRoutes.ts
+++ src/routes/characters/public/pubCharacterRoutes.ts
@@ -22,7 +22,10 @@ router.get("/", async (req: Request, res: Response) => {
     const foundChars = await getCharactersController(characterNames);
     res.status(200).json(foundChars);
   } catch (error: any) {
-    res.status(500).send(error.message);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
   }
 });
 
